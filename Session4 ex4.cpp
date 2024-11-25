998bcb7198219b47419869e583256b26a01884e5
lethanhtung22032005 <lethanhtungdeptrai@gmail.com>
2024-11-25 11:20:46 +0700

    Add files via upload

---
+++ Session4 ex4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+
+    printf("Nhap vao so thang (1-12): ");
+    scanf("%d", &month);
+
+
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        printf("Thang %d co 31 ngay.\n", month);
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        printf("Thang %d co 30 ngay.\n", month);
+    } else if (month == 2) {
+        printf("Thang 2 co 28 hoac 29 ngay (nam nhuan).\n");
+    } else {
+        printf("So thang khong hop le.\n");
+    }
+
+    return 0;
+}
+
